f45660b60c27ce04490907def96a8f701a6d0965
Jendrik Poloczek <jendrik.poloczek@madewithtea.com>
2012-05-13 16:47:56 +0200

    added not completed cv ds approach

---
+++ evopy/views/cv_ds_view.py
@@ -0,0 +1,34 @@
+''' 
+This file is part of evopy.
+
+Copyright 2012, Jendrik Poloczek
+
+evopy is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+evopy is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+evopy.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+from math import floor
+
+class CVDSView():
+    def view(\
+        self, generations, next_population, fitness, best_acc,\
+        parameter_C, parameter_gamma, epsilon):
+
+        population = sorted(next_population, key=lambda child : fitness(child))
+        best_fitness = fitness(population[0])
+        best_acc = floor(100 * best_acc)
+
+        print("gen %i - fitness: %f "
+            "C: %.2f - gamma: %.2f - accuracy: %.2f - epsilon: %f" %\
+            (generations, best_fitness, parameter_C, parameter_gamma, best_acc, epsilon))
+       
