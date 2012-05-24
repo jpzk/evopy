75ff52d16c26829c60ac7fc82b34d4e1d2190c67
Jendrik Poloczek <jendrik.poloczek@madewithtea.com>
2012-05-24 11:28:55 +0200

    added a simpler version of TR2 without tangent

---
+++ evopy/problems/simple_sa_sphere_problem.py
@@ -0,0 +1,47 @@
+''' 
+This file is part of evopy.
+
+Copyright 2012, Jendrik Poloczek
+
+evopy is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+evopy is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+evopy.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+from evopy.individuals.selfadaptive_individual import SelfadaptiveIndividual
+from random import random, sample, gauss
+
+class SimpleSASphereProblem():
+
+    _d = 2 
+    _size = 10
+
+    def termination(self, generations, fitness_of_best):
+        return (1 - 1 * pow(10, -2) < fitness_of_best < 1 + 1 * pow(10, -2))
+
+    # return true if solution is valid, otherwise false.
+    def is_feasible(self, x):
+        return sum(map(lambda x : pow(x,2), x.value)) > 1.0
+
+    # return fitness, 0 is best.
+    def fitness(self, x):
+        return sum(map(lambda x : pow(x,2), x.value)) 
+
+    # python generator for inifinte list of parent population
+    def population_generator(self):
+        d = self._d
+        size = self._size
+
+        while(True):
+            value = map(lambda x : ((x * random()) - 0.5) * size *2, [1] * d)
+            yield(SelfadaptiveIndividual(value, d * [1.0]))
+
