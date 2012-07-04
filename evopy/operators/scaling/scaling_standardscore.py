71117b11e7a53a45bdd1776867d4b8bc95f640fb
Jendrik Poloczek <jendrik.poloczek@madewithtea.com>
2012-07-04 12:29:35 +0200

    major bugfix in standardscore scaling

--- evopy/operators/scaling/scaling_standardscore.py
+++ evopy/operators/scaling/scaling_standardscore.py
@@ -17,6 +17,7 @@ You should have received a copy of the GNU General Public License along with
 evopy.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
+import pdb
 from numpy import array
 from copy import deepcopy
 
@@ -25,13 +26,25 @@ class ScalingStandardscore():
 
     def setup(self, individuals):
         values = map(lambda i : i.value, individuals)
-        iarray = array(values)
-        self._mean = iarray.mean()
-        self._std = iarray.std()
+        dimensions = len(values[0])
+       
+        self._mean = []
+        self._std = []
+        for d in range(0, dimensions):
+            vals = map(lambda u : u[d], values)
+            self._mean.append(array(vals).mean())
+            self._std.append(array(vals).std())
 
     def scale(self, individualx):
         individual = deepcopy(individualx)
         val = individual.value
-        scaled = map(lambda x : (x - self._mean) / self._std, val)
-        individual.value = scaled        
-        return individual 
+        dimensions = len(val)
+    
+        scaled_value = []
+        for d in range(0, dimensions):
+            old_value = val[d]
+            scaled_value.append((old_value - self._mean[d])/ self._std[d])
+
+        individual.value = scaled_value
+        return individual
+
