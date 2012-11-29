c81f35dd9bf4405898e477b2903dff4a688e6156
jpzk <jendrik.poloczek@madewithtea.com>
2012-11-29 20:35:06 +0100

    added example experiment parameter c

---
+++ evopy/examples/experiments/parameter_c_dsessvc/latex.py
@@ -0,0 +1,94 @@
+''' 
+This file is part of evopy.
+
+Copyright 2012 - 2013, Jendrik Poloczek
+
+evopy is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+evopy is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+evopy.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+from sys import path
+path.append("../../../..")
+
+from pickle import load 
+from copy import deepcopy
+from numpy import matrix, log10, log2, array
+from scipy.stats import wilcoxon 
+from itertools import chain
+
+from evopy.strategies.ori_dses_svc_repair import ORIDSESSVCR
+from evopy.strategies.ori_dses_svc import ORIDSESSVC
+from evopy.strategies.ori_dses import ORIDSES
+from evopy.simulators.simulator import Simulator
+
+from evopy.problems.sphere_problem_origin_r1 import SphereProblemOriginR1
+from evopy.problems.sphere_problem_origin_r2 import SphereProblemOriginR2
+from evopy.problems.schwefels_problem_26 import SchwefelsProblem26
+from evopy.problems.tr_problem import TRProblem
+
+from evopy.metamodel.dses_svc_linear_meta_model import DSESSVCLinearMetaModel
+from sklearn.cross_validation import KFold
+from evopy.operators.scaling.scaling_standardscore import ScalingStandardscore
+from evopy.operators.scaling.scaling_dummy import ScalingDummy
+from evopy.metamodel.cv.svc_cv_sklearn_grid_linear import SVCCVSkGridLinear
+
+from evopy.operators.termination.or_combinator import ORCombinator
+from evopy.operators.termination.accuracy import Accuracy
+from evopy.operators.termination.generations import Generations
+from evopy.operators.termination.convergence import Convergence 
+
+from setup import * 
+
+cfile = file("output/parameterc_file.save", "r")
+parameter_c = load(cfile)
+
+# statistics
+variable_names = ['min', 'max']
+variables = {}
+for variable in variable_names:
+    variables[variable] = create_problem_optimizer_map(0.0)
+
+filter_none = lambda x : type(x) != type(None)
+
+for problem in problems:
+    for optimizer in optimizers[problem]:
+        cs = parameter_c[problem][optimizer]
+        cs = list(chain.from_iterable(cs))
+
+        variables['min'][problem][optimizer] = log2(min(filter(filter_none, cs)))
+        variables['max'][problem][optimizer] = log2(max(filter(filter_none, cs)))
+
+results = file("output/results.tex","w")
+lines = [
+    "\\begin{tabularx}{\\textwidth}{l X X}\n", 
+    "\\toprule\n", 
+    "\\textbf{Problem} & Minimum & Maximum \\\\\n",
+    "\midrule\n",
+    "Kugel R. 1 & $2^{%i}$ & $2^{%i}$ \\\\\n"\
+        % (variables['min'][SphereProblemOriginR1][get_method_SphereProblemR1_svc],\
+        variables['max'][SphereProblemOriginR1][get_method_SphereProblemR1_svc]),\
+    "Kugel R. 2 & $2^{%i}$ & $2^{%i}$ \\\\\n"\
+        % (variables['min'][SphereProblemOriginR2][get_method_SphereProblemR2_svc],\
+        variables['max'][SphereProblemOriginR2][get_method_SphereProblemR2_svc]),\
+    "TR2 & $2^{%i}$ & $2^{%i}$ \\\\\n"\
+        % (variables['min'][TRProblem][get_method_TR_svc],\
+        variables['max'][TRProblem][get_method_TR_svc]),\
+    "2.60 mit R. & $2^{%i}$ & $2^{%i}$ \\\\\n"\
+        % (variables['min'][SchwefelsProblem26][get_method_Schwefel26_svc],\
+        variables['max'][SchwefelsProblem26][get_method_Schwefel26_svc]),\
+    "\\bottomrule\n",\
+    "\end{tabularx}\n"]
+
+results.writelines(lines)
+results.close()
+
