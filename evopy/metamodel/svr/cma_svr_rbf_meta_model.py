c73bd980a872f7a0d20d0b53dea5d6a85c4c0278
Jendrik Poloczek <jendrik.poloczek@uni-oldenburg.de>
2013-04-30 10:58:28 +0200

    added CMA-ES variant with SVR surrogate model

--- evopy/metamodel/svr/cma_svr_rbf_meta_model.py
+++ evopy/metamodel/svr/cma_svr_rbf_meta_model.py
@@ -17,11 +17,20 @@ You should have received a copy of the GNU General Public License along with
 evopy.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
+import sys
+
+sys.path.append("../../../")
+
 from collections import deque
 
-from sklearn import svm
+from sklearn.svm import SVR
 from sklearn import __version__ as sklearn_version
 
+from sklearn.grid_search import GridSearchCV
+from sklearn.cross_validation import KFold
+from sklearn.cross_validation import LeaveOneOut
+
+from sklearn.cross_validation import KFold
 from numpy import sum, sqrt, mean, arctan2, pi, matrix, sin, cos
 from numpy import matrix, cos, sin, inner, array, sqrt, arccos, pi, arctan2
 from numpy import transpose
@@ -29,84 +38,75 @@ from numpy.random import rand
 from numpy.random import normal
 from numpy.linalg import inv
 
-from meta_model import MetaModel
+from evopy.metamodel.meta_model import MetaModel
 
 class CMASVRRBFMetaModel(MetaModel):
 
-    def __init__(self, window_size, scaling, crossvalidation, repair_mode):
+    def __init__(self, window_size, scaling):
 
-        super(CMASVCLinearMetaModel, self).__init__()
+        super(CMASVRRBFMetaModel, self).__init__()
 
         self._window_size = window_size
         self._scaling = scaling
-        self._training_infeasibles = deque(maxlen = self._window_size)
-        self._crossvalidation = crossvalidation
-        self._repair_mode = repair_mode
 
-        self.logger.add_binding('_selected_feasibles', 'selected_feasibles')
-        self.logger.add_binding('_selected_infeasibles', 'selected_infeasibles')
         self.logger.add_binding('_best_acc', 'best_acc')
         self.logger.add_binding('_best_parameter_C', 'best_parameter_C')
-        self.logger.add_binding('_normal', 'normal')
+        self.logger.add_binding('_best_parameter_gamma',\
+            'best_parameter_gamma')
+
+        self._trained = False
 
     def is_trained():
         return self._trained
 
-    def add_sorted_feasibles(self, feasibles):
-        self._training_feasibles = feasibles
+    def add(self, solutions):
+        self._trainingset = solutions[:self._window_size]
 
-    def add_infeasible(self, infeasible):
-        self._training_infeasibles.append(infeasible)
-
-    def check_feasibility(self, individual):
-        """ Check the feasibility with meta model """
-
-        scaled_individual = self._scaling.scale(individual)
-        prediction = self._clf.predict(scaled_individual.getA1())
-
-        encode = lambda distance : False if distance < 0 else True
-        return encode(prediction)
+    def predict(self, x):
+        return self._svr.predict(x)[0]
 
     def train(self):
-        """ Train a meta model classification with new points, return True
-            if training was successful, False if not enough infeasible points
-            are gathered """
 
-        if(len(self._training_infeasibles) < self._window_size):
-            self._selected_feasibles = None
-            self._selected_infeasibles = None
-            self._best_parameter_C = None
-            self._best_acc = None
-            self._normal = None
-            self.logger.log()
-            return False
+        fitness = lambda (child, fitness) : fitness
+        child = lambda (child, fitness) : child
 
-        cv_feasibles = self._training_feasibles[:self._window_size]
-        cv_infeasibles = [inf for inf in self._training_infeasibles]
+        trainingset = self._trainingset[:self._window_size]
+        trainingset_child = map(child, trainingset)
+        trainingset_fitness = map(fitness, trainingset)
 
-        self._scaling.setup(cv_feasibles + cv_infeasibles)
+        self._scaling.setup(map(child, trainingset))
 
         scale = lambda child : self._scaling.scale(child)
-        scaled_cv_feasibles = map(scale, cv_feasibles)
-        scaled_cv_infeasibles = map(scale, cv_infeasibles)
+        scaled_trainingset_child = map(scale, trainingset_child)
 
-        self._selected_feasibles, self._selected_infeasibles,\
-        self._best_parameter_C, self._best_acc =\
-            self._crossvalidation.crossvalidate(\
-                scaled_cv_feasibles, scaled_cv_infeasibles)
+        cv_method = KFold(self._window_size, 2)
+        gamma_range = [0.0001, 0.000001, 0.0000001]
+        C_range = [2 ** i for i in range(-3, 14, 2)]
 
-        # @todo WARNING maybe rescale training feasibles/infeasibles (!)
-        fvalues = [f.getA1() for f in self._selected_feasibles]
-        ivalues = [i.getA1() for i in self._selected_infeasibles]
+        tuned_parameters = [{
+            'kernel': ['rbf'],
+            'C': C_range,
+            'gamma': gamma_range}]
 
-        points = ivalues + fvalues
-        labels = [-1] * len(ivalues) + [1] * len(fvalues)
+        # Training set
+        X = map(lambda mat : mat.getA1(), scaled_trainingset_child)
+        y = trainingset_fitness
 
-        self._clf = svm.SVC(kernel = 'linear', C = self._best_parameter_C, tol = 1.0)
-        self._clf.fit(points, labels)
+        grid = GridSearchCV(SVR(kernel='rbf', epsilon = 0.1),
+                param_grid = tuned_parameters, cv=cv_method, verbose=0)
 
-        # Update new basis of meta model
-        self._normal = self.get_normal()
+        grid.fit(X, array(y))
+
+        self._best_parameter_C = grid.best_estimator.C
+        self._best_parameter_gamma = grid.best_estimator.gamma
+        self._best_acc = grid.best_score
+
+        self._svr = SVR(kernel='rbf', epsilon=0.1, C = self._best_parameter_C,\
+            gamma = self._best_parameter_gamma)
+
+        self._svr.fit(X, y)
 
         self.logger.log()
+        self._trained = True
         return True
+
---
+++ evopy/strategies/cconstraint/cmaessvr.py
@@ -0,0 +1,258 @@
+'''
+This file is part of evopy.
+
+Copyright 2012, Jendrik Poloczek
+
+evopy is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+evopy is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+evopy.  If not, see <http://www.gnu.org/licenses/>.
+
+Special thanks to Nikolaus Hansen for providing major part of the CMA-ES code.
+The CMA-ES algorithm is provided in many other languages and advanced versions
+at http://www.lri.fr/~hansen/cmaesintro.html.
+'''
+
+from sys import path
+path.append("../../..")
+
+from copy import deepcopy
+
+from numpy import array, mean, log, eye, diag, transpose
+from numpy import identity, matrix, dot, exp, zeros, ones, sqrt
+from numpy.random import normal, rand
+from numpy.linalg import eigh, norm
+
+from evopy.strategies.evolution_strategy import EvolutionStrategy
+from evopy.operators.scaling.scaling_standardscore import ScalingStandardscore
+
+class CMAESSVR(EvolutionStrategy):
+
+    description =\
+        "Covariance matrix adaption evolution strategy (CMA-ES) with "\
+        "SVR & continuous constraint handling, adaptive 1/5 penalty rule"
+
+    description_short = "CMA-ES & SVR with adaptive 1/5 penalty rule"
+
+    def __init__(self, mu, lambd, xmean, sigma, gamma, tau, beta, meta_model):
+
+        # initialize super constructor
+        super(CMAESSVR, self).__init__(mu, lambd)
+
+        # initialize CMA-ES specific strategy parameters
+        self._init_cma_strategy_parameters(xmean, sigma)
+
+        # valid solutions
+        self._valid_solutions = []
+
+        # statistics
+        self.logger.add_const_binding('_xmean', 'initial_xmean')
+        self.logger.add_const_binding('_sigma', 'initial_sigma')
+
+        self.logger.add_binding('_D', 'D')
+        self.logger.add_binding('_C', 'C')
+        self.logger.add_binding('_B', 'B')
+
+        # log constants
+        self.logger.const_log()
+
+        # initialize gamma
+        self._gamma = gamma
+        self._tau = tau
+
+        # SVC Metamodel
+        self._meta_model = meta_model
+        self._meta_model_trained = False
+        self._beta = beta
+
+    def _init_cma_strategy_parameters(self, xmean, sigma):
+
+        # dimension of objective function
+        N = xmean.size
+        self._xmean = xmean
+        self._sigma = sigma
+
+        # recombination weights
+        self._weights = [log(self._mu + 0.5) - log(i + 1) for i in range(self._mu)]
+
+        # normalize recombination weights array
+        self._weights = [w / sum(self._weights) for w in self._weights]
+
+        # variance-effectiveness of sum w_i x_i
+        self._mueff = sum(self._weights) ** 2 / sum(w ** 2 for w in self._weights)
+
+        # time constant for cumulation for C
+        self._cc = (4 + self._mueff / N) / (N + 4 + 2 * self._mueff / N)
+
+        # t-const for cumulation for sigma control
+        self._cs = (self._mueff + 2) / (N + self._mueff + 5)
+
+        # learning rate for rank-one update of C
+        self._c1 = 2 / ((N + 1.3) ** 2 + self._mueff)
+
+        # and for rank-mu update
+        term_a = 1 - self._c1
+        term_b = 2 * (self._mueff - 2 + 1 / self._mueff) / ((N + 2) ** 2 + self._mueff)
+        self._cmu = min(term_a, term_b)
+
+        # damping for sigma, usually close to 1
+        self._damps = 2 * self._mueff / self._lambd + 0.3 + self._cs
+
+        # evolution paths for C and sigma
+        self._pc = zeros(N)
+        self._ps = zeros(N)
+
+        # B-matrix of eigenvectors, defines the coordinate system
+        self._B = identity(N)
+
+        # diagonal matrix of eigenvalues (sigmas of axes)
+        self._D = ones(N)  # diagonal D defines the scaling
+
+        # covariance matrix, rotation of mutation ellipsoid
+        self._C = identity(N)
+        self._invsqrtC = identity(N)  # C^-1/2
+
+        # approx. norm of random vector
+        self._norm = sqrt(N) * (1.0 - (1.0/(4*N)) + (1.0/21*(N**2)))
+
+        ### FIRST RUN
+        self._D, self._B = eigh(self._C)
+        self._B = matrix(self._B)
+        self._D = [d ** 0.5 for d in self._D]
+
+        invD = diag([1.0/d for d in self._D])
+        self._invsqrtC = self._B * invD * transpose(self._B)
+
+    def ask_pending_solutions(self):
+        """ ask pending solutions; solutions which need a checking for\
+            true feasibility """
+        if self._meta_model_trained:
+            beta = 5
+            todo = self._lambd - beta
+        else:
+            beta = 0
+            todo = self._lambd
+
+        pending_solutions = []
+        while(len(pending_solutions) < todo):
+            normals = transpose(matrix([normal(0.0, d) for d in self._D]))
+            value = self._xmean + transpose(self._sigma * self._B * normals)
+            pending_solutions.append(value)
+
+        self._mm_solutions = []
+        while(len(self._mm_solutions) < beta):
+            normals = transpose(matrix([normal(0.0, d) for d in self._D]))
+            value = self._xmean + transpose(self._sigma * self._B * normals)
+            self._mm_solutions.append(value)
+
+        return pending_solutions
+
+    def tell_fitness_penalty(self, informations):
+        fitness = lambda (child, fitness) : fitness
+        child = lambda (child, fitness) : child
+        penalty = lambda (child, fitness, penalty) : penalty
+        project = lambda (child, fitness, penalty) : (child, fitness)
+
+        # evaluate on _mm_solutions on surrogate model
+        mm_fitnesses = []
+        for solution in self._mm_solutions:
+            t = (solution, self._meta_model.predict(solution))
+            mm_fitnesses.append(t)
+
+        # update CMA-ES specific parameters with penalty values
+        N = self._xmean.size
+        oldxmean = deepcopy(self._xmean)
+
+        addpenalty = lambda (child, fitness, penalty) :\
+                (child, fitness + self._gamma * penalty, penalty)
+
+        fitnessespenalty = map(project, map(addpenalty, informations))
+
+        self._meta_model.add(sorted(fitnessespenalty, key = fitness))
+        self._meta_model.train()
+        self._meta_model_trained = True
+
+        # merge fitnesses and mm_fitnesses
+        merged_fitnesses = fitnessespenalty + mm_fitnesses
+
+        sorted_merged_fitnesses = sorted(merged_fitnesses, key = fitness)[:self._mu]
+        sorted_merged_children = map(child, sorted_merged_fitnesses)
+
+        # new xmean
+        values = sorted_merged_children
+
+        self._xmean = matrix([[0.0 for i in range(0,N)]])
+        weighted_values = zip(self._weights, values)
+        for weight, value in weighted_values:
+            self._xmean += weight * value
+
+        # cumulation: update evolution paths
+        y = self._xmean - oldxmean
+        z = dot(self._invsqrtC, y.T) # C**(-1/2) * (xnew - xold)
+
+        # normalizing coefficient c and evolution path sigma control
+        c = (self._cs * (2 - self._cs) * self._mueff) ** 0.5 / self._sigma
+        self._ps = (1 - self._cs) * self._ps + c * z
+
+        # normalizing coefficient c and evolution path for rank-one-update
+        # without hsig (!)
+        c = (self._cc * (2 - self._cc) * self._mueff) ** 0.5 / self._sigma
+        self._pc = (1 - self._cc) * self._pc + c * y
+
+        # adapt covariance matrix C
+        # rank one update term
+        term_cov1 = self._c1 * (transpose(matrix(self._pc)) * matrix(self._pc))
+
+        # ranke mu update term
+        valuesv = [(value - oldxmean) / self._sigma for value in values]
+        term_covmu = self._cmu *\
+            sum([self._weights[i] * (transpose(matrix(valuesv[i])) *\
+            matrix(valuesv[i]))\
+            for i in range(0, self._mu)])
+
+        self._C = (1 - self._c1 - self._cmu) * self._C + term_cov1 + term_covmu
+
+        # update global sigma by comparing evolution path
+        # with approx. norm of random vector
+        self._sigma *= exp(self._cs / self._damps) *\
+            ((norm(self._ps.getA1()) / self._norm) - 1)
+
+        ### UPDATE FOR NEXT ITERATION
+        self._valid_solutions = []
+
+        ### STATISTICS
+        self._selected_children = values
+        self._best_child, self._best_fitness = sorted_merged_fitnesses[0]
+        self._worst_child, self._worst_fitness = sorted_merged_fitnesses[-1]
+
+        fitnesses = map(fitness, sorted_merged_fitnesses)
+        self._mean_fitness = array(fitnesses).mean()
+
+        # log all bindings
+        self.logger.log()
+        self._count_constraint_infeasibles = 0
+
+        self._D, self._B = eigh(self._C)
+        self._B = matrix(self._B)
+        self._D = [d ** 0.5 for d in self._D]
+
+        invD = diag([1.0/d for d in self._D])
+        self._invsqrtC = self._B * invD * transpose(self._B)
+
+        return True
+
+    def ask_best_solution(self):
+        return self._best_child, self._best_fitness
+
+    def ask_valid_solutions(self):
+        return self._valid_solutions
+
+
