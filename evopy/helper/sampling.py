e1afb764d5125b153fd6589f227946676782ef7b
Jendrik Poloczek <jendrik.poloczek@uni-oldenburg.de>
2013-06-06 11:45:45 +0200

    added parallel support for sampling

--- evopy/helper/sampling.py
+++ evopy/helper/sampling.py
@@ -1,5 +1,24 @@
-from numpy import array
-from itertools import product, groupby
+'''
+This file is part of evopy.
+
+Copyright 2012 - 2013, Jendrik Poloczek
+
+evopy is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+evopy is distributed in the hope that it will be
+useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+evopy.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+from playdoh import map as pmap
+from itertools import product
 from copy import deepcopy
 
 class Sampling(object):
@@ -10,20 +29,20 @@ class Sampling(object):
         algorithm = None
         return (value, error)
 
-    def sample(self, parameters, intervals, stepsizes, args, algorithm, parallel=True):
+    def sample(self, parameters, intervals, stepfuncs, args, algorithm, parallel=False):
 
         steps = {}
         values = {}
         results = []
 
-        for parameter, interval, stepsize in zip(parameters, intervals, stepsizes):
-
+        for parameter, interval, stepfunc in zip(parameters, intervals, stepfuncs):
+            # @todo if stepfunc function does not fit in interval.
             x = 0
             s = interval[0]
             y = interval[0]
 
             while(y < interval[1]):
-                y = stepsize(y)
+                y = stepfunc(y)
                 x += 1
 
             steps[parameter] = x
@@ -32,70 +51,26 @@ class Sampling(object):
             values[parameter].append(s)
             y = s
             for step in xrange(steps[parameter]):
-                y = stepsize(y)
+                y = stepfunc(y)
                 values[parameter].append(y)
 
         iterables = []
         for k, vals in values.iteritems():
             iterables.append([(k, v) for v in vals])
 
-        for combination in product(*iterables):
+        def run_with_parameter(combination):
             xargs = deepcopy(args)
             for parameter in combination:
                 name, value = parameter
                 xargs[name] = value
+            return (xargs, algorithm(xargs))
 
-            results.append((xargs, algorithm(xargs)))
+        if parallel:
+            combinations = [c for c in product(*iterables)]
+            results = pmap(run_with_parameter, combinations)
+        else:
+            for combination in product(*iterables):
+                results.append(run_with_parameter(combination))
 
         return results
 
-
-"""
-args = {}
-parameters = ['a','b']
-intervals = [[-10, 10], [-10, 10]]
-stepsizes = [1,1]
-
-def f(args):
-    return args['a'] * args['b']
-
-sam = Sampling()
-res = sam.sample(parameters, intervals, stepsizes, args, f)
-
-values, steps = {}, {}
-for parameter, interval, stepsize in zip(parameters, intervals, stepsizes):
-    diff = interval[1] - interval[0]
-    if(diff % stepsize > 0):
-        raise Exception("Steps dont fit into interval")
-    steps[parameter] = int(diff / stepsize)
-    values[parameter] = []
-    for step in xrange(steps[parameter]):
-        value = interval[0] + step * stepsize
-        values[parameter].append(value)
-
-A = array([values['a']] * len(values['a']))
-B = array([values['b']] * len(values['b'])).T
-C = []
-
-for rowA, rowB in zip(A,B):
-    C_row = []
-    for valA, valB in zip(rowA, rowB):
-        valC = filter(lambda h : h[0] == {'a':valA, 'b':valB}, res)[0][1]
-        C_row.append(valC)
-    C.append(C_row)
-
-C = array(C)
-
-from mpl_toolkits.mplot3d import axes3d
-import matplotlib.pyplot as plt
-import numpy as np
-import time
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-plt.xlabel("A")
-plt.ylabel("B")
-
-wframe = ax.plot_wireframe(A, B, C, rstride=1, cstride=1)
-plt.show()
-"""
